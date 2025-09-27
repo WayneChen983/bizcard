215bdaac044f100f459df0366e96eac08ee1dc7f
瑋 <tw466376@gmail.com>
2025-09-27 05:11:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1552242347-abca1",
+  "appId": "1:1003534417002:web:793e2e9b2df0737ad41e13",
+  "apiKey": "AIzaSyDtCLcoZG2FfkDZHwJ1sw2sdZMvqbng3hk",
+  "authDomain": "studio-1552242347-abca1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1003534417002"
+};
