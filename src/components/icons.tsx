bd25e3c115752a71d13e45f879f468622e53ce42
瑋 <tw466376@gmail.com>
2025-09-27 04:59:50 +0000

    但是此網址沒東西

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -24,12 +24,7 @@ export const AppLogo = (props: SVGProps<SVGSVGElement>) => (
 );
 
 export const Icons = {
-    home: (props: LucideProps) => (
-        <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <rect width="8" height="18" x="3" y="3" rx="1" />
-            <rect width="8" height="18" x="13" y="3" rx="1" />
-        </svg>
-    ),
+    home: Home,
     camera: Camera,
     settings: Settings,
 };
